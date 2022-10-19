7f528f8c9c821e82c2685f1a7ecb680bf7841ee0
Paul Crane <paul@acheronsoft.com>
2022-10-19 14:59:51 -0500

    Add function isLatin
    Not planning to support looking up kanji on kana input
    so this function will check for those characters and
    return a boolean result.

--- cmd/jisho/main.go
+++ cmd/jisho/main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+	"fmt"
+	"unicode"
+
 	"github.com/Soulsbane/jisho/pkg/jisho"
 	"github.com/alexflint/go-arg"
 )
+
+func isLatin(word string) bool {
+	for _, letter := range word {
+		// Kanji and Kana are not supported so bail out early
+		if !unicode.In(letter, unicode.Latin) {
+			return false
+		}
+	}
+
+	return true
+}
 
 func main() {
 	var args ProgramArgs
 
 	arg.MustParse(&args)
-	jisho.LookupWord(args.Word, args.ListAll)
+
+	if isLatin(args.Word) {
+		jisho.LookupWord(args.Word, args.ListAll)
+	} else {
+		fmt.Println("Kanji and Kana not supported!")
+	}
 }
