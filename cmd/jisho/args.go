920c347c5134d1ef6e637cc7776dc4c2db0134f5
Paul Crane <paul@acheronsoft.com>
2022-12-09 23:40:10 -0600

    Add description to CLI output

--- cmd/jisho/args.go
+++ cmd/jisho/args.go
@@ -4,3 +4,7 @@ type ProgramArgs struct {
 	Word    string `arg:"positional, required"`
 	ListAll bool   `arg:"-a,--list-all" default:"false"`
 }
+
+func (args ProgramArgs) Description() string {
+	return "Gets the definition, reading and kanji for the given romaji input"
+}
